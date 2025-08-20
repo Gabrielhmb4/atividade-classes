a5965d4908514bafcf3c4995f2e25d7099421859
Gabrielhmb4 <gabrielhmbrandao@gmail.com>
2025-08-20 16:01:04 -0300

    Create classe.py

---
+++ classe.py
@@ -0,0 +1,17 @@
+class Contato:  
+  def__init__(self, nome, email):
+    self.nome = nome 
+    self.email = email 
+
+    def atualizar_nome(self,novo_nome):
+      self.nome = novo_nome 
+
+    def atualizar_email(self, novo_email):
+        self.email = novo_email 
+
+    def to_adict(self): 
+      return {"nome": self.nome, "email": self.email} 
+
+
+    delf __str__(self):
+      return f"{self.nome} ({self.email})"
